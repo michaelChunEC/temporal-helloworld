c234d8701056337d52eb410f2fca4e6d51078a5c
michaelChunEC <michael@easycrypto.ai>
2022-11-29 10:24:14 +1300

    feat: hello world init

---
+++ worker/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+
+	"go.temporal.io/sdk/client"
+	"go.temporal.io/sdk/worker"
+
+	"github.com/michaelEC/temporal-helloworld"
+)
+
+func main() {
+	// The client and worker are heavyweight objects that should be created once per process.
+	c, err := client.Dial(client.Options{
+		HostPort:  "your-custom-namespace.tmprl.cloud:7233",
+		Namespace: "your-custom-namespace",
+	})
+	if err != nil {
+		log.Fatalln("Unable to create client", err)
+	}
+	defer c.Close()
+
+	w := worker.New(c, "hello-world", worker.Options{})
+
+	w.RegisterWorkflow(helloworld.Workflow)
+	w.RegisterWorkflow(helloworld.Workflow2)
+	w.RegisterActivity(helloworld.Activity)
+
+	err = w.Run(worker.InterruptCh())
+	if err != nil {
+		log.Fatalln("Unable to start worker", err)
+	}
+}
